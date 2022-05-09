7ef7224d762074ef58abb4b333ed692c44a7de39
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-05-09 22:25:02 +0300

    update

--- currency.js
+++ currency.js
@@ -1,22 +1,27 @@
-const data = '{ "distance": { "unit": "km", "value": "qq"}, "convert_to": "ft" }';
-const rules = '{"imperialUnits": { "in": 25.4, "ft": 304.8, "yd": 914.4 },"metriclUnits": { "mm": 1, "cm": 10, "m": 1000, "km": 1000000}}';
+const data =
+  '{ "distance": { "unit": "km", "value": "qq"}, "convert_to": "ft" }';
+const rules =
+  '{"imperialUnits": { "in": 25.4, "ft": 304.8, "yd": 914.4 },"metriclUnits": { "mm": 1, "cm": 10, "m": 1000, "km": 1000000}}';
 
 function converter(data, rules) {
   const { unit, value } = JSON.parse(data).distance;
   const { convert_to } = JSON.parse(data);
-  const {imperialUnits, metriclUnits} = JSON.parse(rules);
+  const { imperialUnits, metriclUnits } = JSON.parse(rules);
 
-  const inputUnitIsImperial = Object.keys(imperialUnits).includes(unit);
-  
-  const multiplier = inputUnitIsImperial
+  const isImperialUnit = Object.keys(imperialUnits).includes(unit);
+
+  const multiplier = isImperialUnit
     ? imperialUnits[unit] / metriclUnits[convert_to]
-    : metriclUnits[unit] / imperialUnits[convert_to]
-  
+    : metriclUnits[unit] / imperialUnits[convert_to];
+
   if (!multiplier || !Number(value)) {
     alert("Can't convert. Please check the units");
-    return
-}
-const result = { unit: convert_to, value: (Number(value) * multiplier).toFixed(2) };
+    return;
+  }
+  const result = {
+    unit: convert_to,
+    value: (Number(value) * multiplier).toFixed(2),
+  };
 
   return console.log(JSON.stringify(result));
 }
