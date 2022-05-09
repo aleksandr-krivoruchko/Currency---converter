a68c26fbb4f5ba0c1202c193056ebbdeafe74c42
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-05-09 09:19:16 +0300

    update

--- currency.js
+++ currency.js
@@ -1,41 +1,24 @@
-const json =
-  '{ "distance": { "unit": "m", "value": 100 }, "convert_to": "ft" }';
+const data = '{ "distance": { "unit": "km", "value": 2}, "convert_to": "yd" }';
+const rules = '{"imperialUnits": { "in": 25.4, "ft": 304.8, "yd": 914.4 },"metriclUnits": { "mm": 1, "cm": 10, "m": 1000, "km": 1000000}}';
 
-const rule = {
-  ft: { km: 3281, m: 3.281, cm: 0.032808, mm: 0.003281 },
-  yd: { km: 1094, m: 1.094, cm: 0.010936, mm: 0.001094 },
-  in: { km: 39370, m: 39.37, cm: 0.393701, mm: 0.03937 },
-};
+function converter(data, rules) {
+  const { unit, value } = JSON.parse(data).distance;
+  const { convert_to } = JSON.parse(data);
+  const {imperialUnits, metriclUnits} = JSON.parse(rules);
 
-const imperialUnits = Object.keys(rule);
-const metriclUnits = Object.keys(Object.values(rule)[0]);
+  const inputUnitIsImperial = Object.keys(imperialUnits).includes(unit);
 
-function converter(json) {
-  const data = JSON.parse(json);
-  const { unit, value } = data.distance;
-  const { convert_to } = data;
-  const inputUnits = Object.keys(rule).includes(unit);
-  const exitUnits = Object.keys(rule).includes(convert_to);
-
-  let result = { unit: convert_to, value: null };
-
-  if (
-    (imperialUnits.includes(unit) && imperialUnits.includes(convert_to)) ||
-    (metriclUnits.includes(unit) && metriclUnits.includes(convert_to))
-  ) {
-    alert("Attempt to convert in one measurement system");
-    return;
-  }
-
-  if (!inputUnits && !exitUnits) {
-    console.log(inputUnits);
-    alert("No such unit of measure");
-    return;
-  }
-  result.value = inputUnits
-    ? (Number(value) / rule[unit][convert_to]).toFixed(2)
-    : (Number(value) * rule[convert_to][unit]).toFixed(2);
+  const multiplier = inputUnitIsImperial
+    ? imperialUnits[unit] / metriclUnits[convert_to]
+    : metriclUnits[unit] / imperialUnits[convert_to]
+  
+  if (!multiplier || !Number(value)) {
+    alert("Can't convert. Please check the units");
+    return
+}
+const result = { unit: convert_to, value: (Number(value) * multiplier).toFixed(2) };
 
   return console.log(JSON.stringify(result));
 }
-converter(json);
+
+converter(data, rules);
