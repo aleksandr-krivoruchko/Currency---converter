f46119bbbebbc907c28a9766d520d4d66a3481d8
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-05-11 22:56:03 +0300

    qqq

--- sort.js
+++ sort.js
@@ -1,23 +1,24 @@
 // const incomingData ='{"data": [{"user": "mike@mail.com", "rating": 20, "disabled": false},{"user": "greg@mail.com", "rating": 14, "disabled": false},{"user": "john@mail.com", "rating": 25, "disabled": true},{"user": "johny@mail.com", "rating": 25, "disabled": true}],"condition": {"exclude": [{"disabled": true}], "sort_by": ["rating"]}}';
-const incomingData ='{"data": [{"name": "John", "email": "john2@mail.com"},{"name": "John", "email": "john1@mail.com"},{"name": "Jane", "email": "jane@mail.com"},{"name": "John", "email": "john3@mail.com"}],"condition": {"include": [{"name": "John"}], "sort_by": ["email"]}}';
-
+const incomingData =
+  '{"data": [{"name": "John", "email": "john2@mail.com"},{"name": "John", "email": "john1@mail.com"},{"name": "Jane", "email": "jane@mail.com"},{"name": "John", "email": "john3@mail.com"}],"condition": {"include": [{"name": "John"}], "sort_by": ["email"]}}';
 
 function dataSorting(incomingData) {
-
   const { data, condition } = JSON.parse(incomingData);
 
-   let filter = {}; //массив вытащенных ключей фильтра 
-   
-   const conditionName = Object.keys(condition)[0]; //ключ массива фильтрации 
-   
-//перебираем массив значений фильтрации и заполняем обьект filter
-    Object.values(condition[conditionName]).forEach((item) => filter[Object.keys(item)] = item[Object.keys(item)]); 
-   
-  const conditionKey = Object.keys(filter)[0]; // ключ фильтрации 
-  const conditionValue = Object.values(filter)[0]; //значение фильтрации 
-   const sortBy = condition.sort_by[0]; //значение сортировки 
+  let filter = {}; //массив вытащенных ключей фильтра
 
-   // Сначала фильтруем по значению фильтрации, потом сортируем по значению сортировки
+  const conditionName = Object.keys(condition)[0]; //ключ массива фильтрации
+
+  //Перебираем массив значений фильтрации и заполняем обьект filter
+  Object.values(condition[conditionName]).forEach(
+    (item) => (filter[Object.keys(item)] = item[Object.keys(item)])
+  );
+
+  const conditionKey = Object.keys(filter)[0]; // ключ фильтрации
+  const conditionValue = Object.values(filter)[0]; //значение фильтрации
+  const sortBy = condition.sort_by[0]; //значение сортировки
+
+  // Сначала фильтруем по значению фильтрации, потом сортируем по значению сортировки
   const result = data
     .filter((item) =>
       conditionName === "include"
--- victorina.js
+++ victorina.js
@@ -1,101 +1,122 @@
-let questions = [{
-    id: '1',
-    question: 'What is your marital status?',
+let data = [
+  {
+    id: "1",
+    question: "What is your marital status?",
     answer1: {
-      text: 'Single',
-      next_question: '1.1'
+      text: "Single",
+      next_question: "1.1",
     },
     answer2: {
-      text: 'Married',
-      next_question: '1.2'
-    }
+      text: "Married",
+      next_question: "1.2",
+    },
   },
 
   {
-    id: '1.1',
-    question: 'Are you planning on getting married next year?',
+    id: "1.1",
+    question: "Are you planning on getting married next year?",
     answer1: {
-      text: 'Yes',
-      next_question: '1.1.1'
+      text: "Yes",
+      next_question: "1.1.1",
     },
     answer2: {
-      text: 'No',
-      next_question: '2.1.2'
-    }
+      text: "No",
+      next_question: "2.1.2",
+    },
   },
 
   {
-    id: '1.2',
-    question: 'How long have you been married?',
+    id: "1.2",
+    question: "How long have you been married?",
     answer1: {
-      text: 'Less than a year',
-      next_question: '1.2.1'
+      text: "Less than a year",
+      next_question: "1.2.1",
     },
     answer2: {
-      text: 'More than a year',
-      next_question: '1.2.2'
-    }
-   },
-  
+      text: "More than a year",
+      next_question: "1.2.2",
+    },
+  },
+
   {
-    id: '1.2.2',
-    question: 'Have you celebrated your one year anniversary?',
+    id: "1.2.2",
+    question: "Have you celebrated your one year anniversary?",
     answer1: {
-      text: 'Yes',
-      next_question: '1.2.2.1'
+      text: "Yes",
+      next_question: "1.2.2.1",
     },
     answer2: {
-      text: 'No',
-      next_question: '1.2.2.2'
-    }
-  }
+      text: "No",
+      next_question: "1.2.2.2",
+    },
+  },
 ];
 
 function questionnaire(data) {
-//[{"What is your marital status?": "Single"},
-// {"Are you planning on getting married next year?": "Yes/No"}],
-// [{"What is your marital status?": "Married"},
-// {"How long have you been married?": "Less than a year"}],
-// [{"What is your marital status?": "Married"},
-// {"How long have you been married?": "More than a year"},
-// {"Have you celebrated your one year anniversary?": "Yes/No"}],
+  //[{"What is your marital status?": "Single"},
+  // {"Are you planning on getting married next year?": "Yes/No"}],
+  // [{"What is your marital status?": "Married"},
+  // {"How long have you been married?": "Less than a year"}],
+  // [{"What is your marital status?": "Married"},
+  // {"How long have you been married?": "More than a year"},
+  // {"Have you celebrated your one year anniversary?": "Yes/No"}],
 
+  let result = { paths: { number: 0, list: [] } };
+  const tree = {};
+  console.log(tree);
 
+  for (const item of data) {
+    tree[item.question] = [item.answer1.text, item.answer2.text];
+  }
 
-//    console.log(tree);
-//          const path1 = [];
-// const path2 = [];
-//    for (const item of data) {
-//       if (item.id === '1') {
-//          path1.push({ [item.question]: item.answer1.text })
-//       } else { path2.push({ [item.question]: item.answer2.text }) }
-      
-//       if (item.id === '1.1') {
-//          path1.push({[item.question]:`${item.answer1.text}/${item.answer2.text}`})
-//       } else { path2.push({ [item.question]: `${item.answer1.text}/${item.answer2.text}`}) }
-//    }
-// console.log(path1);
-// console.log(path2);
-//    console.log(result);
-      let result = { paths: { number: 0, list: [] } }
+  const firstKey = Object.keys(tree)[0];
 
-   const tree = {};
+  console.log(firstKey);
+  console.log(tree[firstKey]);
 
-for (const item of data) {
-   //  tree[item.question] = `${item.answer1.text}/${item.answer2.text}`;
-       tree[item.id] = {[item.question]:`${item.answer1.text}/${item.answer2.text}`};
-   }
-   console.log(tree);
-   // console.log(Object.values(questions)[0]);
-   // console.log(Object.keys(tree)[0]);
-const array = Object.entries(tree)
+  const func = (firstKey, firstArr) => {
+    firstArr.push(firstKey);
 
-for (let index = 0; index < array.length; index++) {
-   const element = array[index];
-   if (element[0].includes(1))
-   result.paths.list.push(element)
-   console.log(element[0]);
-}
+    for (const item of tree[firstKey]) {
+      if (!tree[item]) {
+        result.paths.list.push([...firstArr, item]);
+        //   console.log([...firstArr, item]);
+      } else {
+        func(item, [...firstArr]);
+      }
+    }
+  };
+
+  func(firstKey, []);
+  console.log(result.paths.list);
 }
 
-questionnaire(questions)
+questionnaire(data);
+// ==================================================
+
+// ==========================================================
+// const tree = {};
+
+// for (const item of data) {
+//   tree[item.id] = [item.answer1.next_question, item.answer2.next_question];
+// }
+// console.log(tree);
+
+// const firstKey = Object.keys(tree)[0];
+
+// console.log(firstKey);
+// console.log(tree[firstKey]);
+
+// const func = (firstKey, firstArr) => {
+//   firstArr.push(firstKey);
+
+//   for (const item of tree[firstKey]) {
+//     if (!tree[item]) {
+//       console.log([...firstArr, item]);
+//     } else {
+//       func(item, [...firstArr]);
+//     }
+//   }
+// };
+
+// func(firstKey, []);
